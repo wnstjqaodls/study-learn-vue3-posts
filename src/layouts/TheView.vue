96bd1a0f72de1b60c0f50965c108ff4831d25c91
subkim <wnstjqaodls@gmail.com>
2025-02-02 17:23:43 +0900

    first commit

---
+++ src/layouts/TheView.vue
@@ -0,0 +1,15 @@
+<template>    
+  <main>
+    <div class="container py-4">
+      <h2>Hellow Vue3</h2>
+    </div>
+  </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
